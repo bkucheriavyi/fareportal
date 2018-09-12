63e4b6f681988160dd5e7ca0bd51ba3e5f0ffbf7
Bohdan <bohdan.kucheriavuy@gmail.com>
2018-09-12 10:29:42 +0200

    Update git ignore.\nAdded missing files

---
+++ FPT.Console/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace FPT.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+            string beverageId = args[0];
+            string additives = args[1];
+        }
+    }
+
+    public class Beverage
+    {
+        public int Id {get;set}
+        public string Name {get;set;}
+        public double Price {get;set;}
+        public DringType DrinkType {get;set}
+    }
+
+    [Flags]
+    public enum DrinkType : int
+    {
+        Coffee = 0,
+        Tea = 1
+    }
+    
+    public class Additive
+    {
+        public int Id {get;set;}
+        public string Name {get;set;}
+        public double Price {get;set;}
+        public enum DrinkType AdditiveFor {get; set;}
+    }
+}
